3a52e2fe73d06adca8d3993f6e08bfec2990bb4a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-17 09:37:31 -0400

    commit tree ceeb237496a99a0a6255ea62cea11c71d65f857e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Liz",
+    streetAddress: "12 Armoury Drive"
+};
+
+const updateEmployeeWithKeyAndValue = (employee, name, newName) => {
+    const newEmployee = {...employee}
+    newEmployee[name] = newName;
+    return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, name, newName) => {
+    employee[name] = newName;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, name) => {
+    const delEmployee = {...employee}
+    delete delEmployee.name;
+    return delEmployee;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, name) => {
+    delete employee.name;
+    return employee;
+}
